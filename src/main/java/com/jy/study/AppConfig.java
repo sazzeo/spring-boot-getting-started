ed14fac161b80a41027f1ab906a6f90e15fbf116
sazzeo <imjee02@gmail.com>
2022-06-09 16:00:35 +0900

    AppConfig를 이용한 DI 학습

--- src/main/java/com/jy/study/AppConfig.java
+++ src/main/java/com/jy/study/AppConfig.java
@@ -1,6 +1,8 @@
 package com.jy.study;
 
+import com.jy.study.discount.DiscountPolicy;
 import com.jy.study.discount.FixDiscountPolicy;
+import com.jy.study.discount.RateDiscountPolicy;
 import com.jy.study.member.MemberService;
 import com.jy.study.member.MemberServiceImpl;
 import com.jy.study.member.MemoryMemberRepository;
@@ -10,10 +12,22 @@ import com.jy.study.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(getMemberRepository());
     }
 
+
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository() , new FixDiscountPolicy());
+        return new OrderServiceImpl(getMemberRepository() , getDiscountPolicy());
     }
+
+    //굳이 getter만드는 이유: 겹치는 코드 수정시 한번만 수정하기 위해서
+    private MemoryMemberRepository getMemberRepository() {
+        return new MemoryMemberRepository();
+    }
+    private DiscountPolicy getDiscountPolicy() {
+        return new RateDiscountPolicy();
+    }
+
+
+    
 }
